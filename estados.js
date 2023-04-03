1e6983f85798c76d6bd3ac253ce116de66883e5a
aryniceia <aryniceia@gmail.com>
2023-04-03 15:42:01 -0300

    first commit

---
+++ estados.js
@@ -0,0 +1,22 @@
+var sp = 67836.43
+var rj = 36678.66
+var mg = 29229.88
+var es = 27165.48
+var outro = 19849.53
+
+var soma = sp + rj + mg + es + outro;
+console.log(soma)
+
+//percentuais
+
+var psp = ((sp*soma)/100)
+var prj = ((rj*soma)/100)
+var pmg = ((mg*soma)/100)
+var pes = ((es*soma)/100)
+var poutro = ((outro*soma)/100)
+
+console.log('A porcentagem de SP é de:' + psp)
+console.log('A porcentagem de RJ é de:' + prj)
+console.log('A porcentagem de MG é de:' + pmg)
+console.log('A porcentagem de ES é de:' + pes)
+console.log('A porcentagem de outro é de:' + poutro)
